c4361ee5424c46a7891da0201572222819bc38be
git <ahmedsalah511998@gmail.com>
2026-04-09 20:15:09 +0300

    gfjghjghj

--- reports/export_pdf.php
+++ reports/export_pdf.php
@@ -135,7 +135,7 @@ foreach($drivers as $d){
          + $totals[$d['id']]['internet'] 
          + $totals[$d['id']]['other'];
     $detail_total = trim($totals_desc[$d['id']], "; ");
-    $html .= "<td colspan='4'>$sum</td>";
+    $html .= "<td colspan='5'>$sum</td>";
 }
 $html .= "</tr>";
 

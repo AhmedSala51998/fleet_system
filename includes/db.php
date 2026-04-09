49d5b705e695d165010abbaa91a025f7eb161850
git <ahmedsalah511998@gmail.com>
2026-04-09 14:57:25 +0300

    save_database_manage

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost","root","","fleet_system");
+$conn = mysqli_connect("localhost","u552468652_fleet_system","Fleet_system12345","u552468652_fleet_system");
 if(!$conn){
     die("DB Error");
 }

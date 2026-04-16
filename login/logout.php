60590b0551b2b544032fd88cae2953be3267ca39
git <ahmedsalah511998@gmail.com>
2026-04-16 19:26:53 +0300

    jghjghjgh

--- login/logout.php
+++ login/logout.php
@@ -1,4 +1,23 @@
 <?php
 session_start();
+
+// احفظ اللغة قبل ما تمسح السيشن
+if (isset($_SESSION['lang'])) {
+    $lang = $_SESSION['lang'];
+} elseif (isset($_GET['lang'])) {
+    $lang = $_GET['lang'];
+} else {
+    $lang = 'ar';
+}
+
+// امسح السيشن (تسجيل خروج)
+session_unset();
 session_destroy();
-header("Location: login.php");
+
+// (اختياري) لو عايز تحفظ اللغة بعد اللوجين تاني
+session_start();
+$_SESSION['lang'] = $lang;
+
+// تحويل مع تمرير اللغة في الرابط
+header("Location: login.php?lang=" . urlencode($lang));
+exit;
